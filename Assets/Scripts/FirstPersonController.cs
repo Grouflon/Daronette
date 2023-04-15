1c2b647714fd00b8721e44f81d2a672e7ff9097a
Hugo <alterisateur@gmail.com>
2023-04-15 17:40:11 +0200

    Folders and FPController init

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    float Yaxis;
+    float Xaxis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
